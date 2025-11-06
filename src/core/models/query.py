9fdf292b4ebdf8bf41cb2d73df3779eca5b1e843
Fathur <rizkyfathur326@gmail.com>
2025-11-06 15:58:17 +0700

    feat(query): add initial model

--- src/core/models/query.py
+++ src/core/models/query.py
@@ -1,5 +1,16 @@
+from typing import List
 from dataclasses import dataclass
+
+
+@dataclass
+class QueryTree:
+    type: str
+    value: str
+    children: List['QueryTree']
+    parent: 'QueryTree' = None
+
 
 @dataclass
 class ParsedQuery:
-    ...
+    tree: QueryTree
+    query: str
