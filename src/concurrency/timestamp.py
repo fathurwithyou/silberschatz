f535a9ea3112c055800d2375b8debb91e60752e7
Farzi36 <faris361707@gmail.com>
2025-11-13 18:52:28 +0700

    feat : timestamp skeleton

--- src/concurrency/timestamp.py
+++ src/concurrency/timestamp.py
@@ -1,2 +1,18 @@
-class TimestampBasedConcurrencyControl:
-    pass
+from core.concurrency_manager import IConcurrencyControlManager
+
+class TimestampBasedConcurrencyControl(IConcurrencyControlManager):
+    def __init__(self):
+        pass
+
+    def begin_transaction(self, transaction):
+        pass
+
+    def end_transaction(self, transaction):
+        pass
+
+    def log_object(self, row, transaction):
+        pass
+
+    def validate_object(self, row, transaction, action):
+        pass
+    
